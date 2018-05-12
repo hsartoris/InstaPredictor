63b67e05aadad15e032e3ca2f9df84fd4c1194e5
Hayden Sartoris <hsartoris@gmail.com>
2018-05-11 20:33:25 -0400

    ok

--- utilities/ProcessFollowCount.py
+++ utilities/ProcessFollowCount.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
         stamp = calendar.timegm(datetime.datetime(int(date[0]), int(date[1]), 
                 int(date[2]), 12, 0, 0).timetuple())
         out[i] = [stamp, int(likes)]
-    np.savetxt(outDir + "followCount_proc", out, delimiter=',', fmt="%i,i")
+    np.savetxt(outDir + "followCount_proc", out, delimiter=',', fmt="%i,%i")
 
