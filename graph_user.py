ab195ed3a9600d4a96fa6569ca9e3c4af6571558
Hayden Sartoris <hsartoris@gmail.com>
2018-05-12 17:31:35 -0400

    me to hell

--- graph_user.py
+++ graph_user.py
@@ -2,43 +2,57 @@ import time, sys, datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdate
 import numpy as np
-from .preprocess import scrape_user
+from preprocess import scrape_user
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: graph_user <userDir>")
         exit()
 
-    userDir = sys.argv[1]
+    userDir = sys.argv[1].strip("/")
     user = userDir.split("/")[-1]
-    if not userDir[-1] == "/": userDir += "/"
+    instaDir = "/".join(userDir.split("/")[:-1]) + "/"
+    userDir += "/"
     js = open(userDir + user + ".json")
     fc = open(userDir + "followCount_proc")
     xfc = []
+    fc_timestamps = []
     yfc = []
     for line in fc.readlines():
         timestamp, followers = line.strip().split(",")
+        fc_timestamps.append(int(timestamp))
         xfc.append(mdate.epoch2num(int(timestamp)))
         yfc.append(int(followers))
     print("Processed", len(xfc), "follower counts")
 
     xPost = []
+    post_timestamps = []
     yPost = []
-    instaDir = "/".join(userDir.split("/")[:-2]) + "/"
     posts, _ = scrape_user(instaDir, user, validateMedia = False)
     for post in posts:
+        post_timestamps.append(post['timestamp'])
         xPost.append(mdate.epoch2num(post['timestamp']))
-        xPost.append(post['likes'])
+        yPost.append(post['likes'])
+    post_timestamps = post_timestamps[::-1]
+    xPost = xPost[::-1]
+    yPost = yPost[::-1]
 
     print("Processed", len(xPost), "posts")
+    for i in range(len(yPost)):
+        if post_timestamps[i] >= fc_timestamps[0]:
+            print("breaking at",i)
+            xPost = xPost[i:]
+            yPost = yPost[i:]
+            break
 
-    fig, ax = plt.subplots()
-    ax.plot_date(xfc, yfc)
-    ax.plot_date(xPos, yPost)
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.plot_date(xfc, yfc)
+    ax2.plot_date(xPost, yPost)
 
-    date_fmt= = '%d-%m-%y'
+    date_fmt = '%d-%m-%y'
     date_formatter = mdate.DateFormatter(date_fmt)
-    ax.xaxis.set_major_formatter(date_formatter)
+    ax1.xaxis.set_major_formatter(date_formatter)
+    ax2.xaxis.set_major_formatter(date_formatter)
     fig.autofmt_xdate()
     plt.show()
 
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,13 @@
-import json, sys, os, shutil
+import json, sys, os, shutil, datetime, calendar, time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
 import numpy as np
 
 userFile = "users.txt.bak"
 
+class Modes():
+    TRAININGDATA = 0
+    ANALYZE = 1
 
 def scrape_user(instaDir, user, validateMedia=True):
     posts = []
@@ -39,16 +44,76 @@ def user_to_data(user, instaDir, user_posts, outDir, ids, idx=0):
         ids.append(post['likes'])
         idx += 1
     return idx
-        
-    
-    
+
+def analyze2(posts):
+    # monday is 0
+    fig, axes = plt.subplots(1,7)
+    user_likes = []
+    yMax = 0
+    for (user, user_posts) in posts:
+        if user == "actuallyaxley" or user == "vmalik7": continue
+        days = []
+        for i in range(7): days.append([[],[]])
+        for post in user_posts:
+            days[time.localtime(post['timestamp'])[6]][0].append(
+                time.localtime(post['timestamp'])[3])
+            days[time.localtime(post['timestamp'])[6]][1].append(
+                post['likes'])
+            if post['likes'] > yMax:
+                yMax = post['likes']
+        user_likes.append(days)
+        for i in range(len(days)):
+            line, = axes[i].plot(user_likes[-1][i][0], user_likes[-1][i][1], 
+                    'o', label=user)
+
+    yMax += 50
+    for ax in axes:
+        ax.set_ylim([0, yMax])
+    axes[0].legend()
+    plt.show()
+
+def analyze(posts):
+    # monday is 0
+    fig, axes = plt.subplots(1,7)
+    hours = [i for i in range(24)]
+    user_likes = []
+    yMax = 0
+    for (user, user_posts) in posts:
+        if not user == "betthole": continue
+        days = []
+        for i in range(7): days.append([0]*24)
+        print(days)
+        for post in user_posts:
+            day = time.localtime(post['timestamp'])[6]
+            hour = time.localtime(post['timestamp'])[3]
+            
+            days[day][hour] += post['likes']
+        if np.max(days) > yMax:
+            yMax = np.max(days)
+        user_likes.append(days)
+        print(hours)
+        print(days[0])
+        for i in range(len(days)):
+            line, = axes[i].plot(hours, user_likes[-1][i], label=user)
+
+    yMax += 50
+    for ax in axes:
+        ax.set_ylim([0, yMax])
+    axes[0].legend()
+    plt.show()
 
 if __name__ == "__main__":
-    if not len(sys.argv) == 3:
+    mode = Modes.ANALYZE
+    if mode == Modes.TRAININGDATA and not len(sys.argv) == 3:
         print("Usage: preprocess.py <instaDir> <outDir>")
         exit()
+    elif mode == Modes.TRAININGDATA:
+        outDir = sys.argv[2] + "/"
+    elif mode == Modes.ANALYZE and not len(sys.argv) == 2:
+        print("Usage: preprocess.py <instaDir>")
+        exit()
+
     instaDir = sys.argv[1] + "/"
-    outDir = sys.argv[2] + "/"
     
     if not os.path.exists(instaDir):
         print("Error: couldn't find instaDir " + instaDir)
@@ -57,11 +122,12 @@ if __name__ == "__main__":
         print("Error: couldn't find user file")
         exit()
 
-    if not os.path.exists(outDir):
-        print("Creating output directory: " + outDir)
-        os.mkdir(outDir)
-    if not os.path.exists(outDir + "images"):
-        os.mkdir(outDir + "images")
+    if mode == Modes.TRAININGDATA:
+        if not os.path.exists(outDir):
+            print("Creating output directory: " + outDir)
+            os.mkdir(outDir)
+        if not os.path.exists(outDir + "images"):
+            os.mkdir(outDir + "images")
     
     f = open(instaDir + userFile)
     users = f.readlines()
@@ -69,20 +135,30 @@ if __name__ == "__main__":
     
     ids = []
     idx = 0
+    posts = []
     for user in users:
         user = user.strip()
         print("Checking user " + user)
         if not os.path.exists(instaDir + user):
             print("Couldn't find dir for " + user + "(" + instaDir + user + ")")
             continue
-        user_posts, missed = scrape_user(instaDir, user)
+        user_posts, missed = scrape_user(instaDir, user,
+                validateMedia=(mode==Modes.TRAININGDATA))
         if user_posts: 
             print("Got", len(user_posts), "for", user + "; missed", missed)
-            idx = user_to_data(user, instaDir, user_posts, outDir, ids, idx)
+            if mode == Modes.TRAININGDATA:
+                idx = user_to_data(user, instaDir, user_posts, outDir, ids, idx)
+            posts.append((user,user_posts))
         else:
             print("No posts returned for user " +  user)
-    print("Processed", idx, "posts; saved to", outDir)
-    ids = np.array(ids).transpose()
-    np.savetxt(outDir + "ids", ids, fmt='%i')
+            continue
+
+    if mode == Modes.TRAININGDATA:
+        print("Processed", idx, "posts; saved to", outDir)
+        ids = np.array(ids).transpose()
+        np.savetxt(outDir + "ids", ids, fmt='%i')
+    elif mode == Modes.ANALYZE:
+        analyze2(posts)
+
         
         
